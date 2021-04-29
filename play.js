20d6ebd90b611adfcd70ec71a5f229589ddda50f
Aidan Shirey <aidan.shirey@gmail.com>
2021-04-28 23:21:04 -0700

    Added More Sets

--- play.js
+++ play.js
@@ -1,5 +1,48 @@
 // Play Key Function
 var path = "Drum Samples/Acoustic/";
+var ipath = "Drum Icons/Acoustic/";
+
+function setIcons(iconpath){
+    var qKey = document.getElementById("qkey").getAttribute("value");
+    document.getElementById("qkey").setAttribute("src", iconpath + qKey + ".png");
+    
+    var wKey = document.getElementById("wkey").getAttribute("value");
+    document.getElementById("wkey").setAttribute("src", iconpath + wKey + ".png");
+    
+    var eKey = document.getElementById("ekey").getAttribute("value");
+    document.getElementById("ekey").setAttribute("src", iconpath + eKey + ".png");
+    
+    var rKey = document.getElementById("rkey").getAttribute("value");
+    document.getElementById("rkey").setAttribute("src", iconpath + rKey + ".png");
+
+    var uKey = document.getElementById("ukey").getAttribute("value");
+    document.getElementById("ukey").setAttribute("src", iconpath + uKey + ".png");
+
+    var iKey = document.getElementById("ikey").getAttribute("value");
+    document.getElementById("ikey").setAttribute("src", iconpath + iKey + ".png");
+
+    var oKey = document.getElementById("okey").getAttribute("value");
+    document.getElementById("okey").setAttribute("src", iconpath + oKey + ".png");
+
+    var pKey = document.getElementById("pkey").getAttribute("value");
+    document.getElementById("pkey").setAttribute("src", iconpath + pKey + ".png");
+
+    var fKey = document.getElementById("fkey").getAttribute("value");
+    document.getElementById("fkey").setAttribute("src", iconpath + fKey + ".png");
+    
+    var gKey = document.getElementById("gkey").getAttribute("value");
+    document.getElementById("gkey").setAttribute("src", iconpath + gKey + ".png");
+    
+    var hKey = document.getElementById("hkey").getAttribute("value");
+    document.getElementById("hkey").setAttribute("src", iconpath + hKey + ".png");
+    
+    var jKey = document.getElementById("jkey").getAttribute("value");
+    document.getElementById("jkey").setAttribute("src", iconpath + jKey + ".png");
+
+    var spaceKey = document.getElementById("spacekey").getAttribute("value");
+    document.getElementById("spacekey").setAttribute("src", iconpath + spaceKey + ".png");
+}
+setIcons(ipath);
 
 function play(samplename){
     var mPath = path + samplename + ".wav";
@@ -65,6 +108,11 @@ document.addEventListener("keydown", function(e){
     }
  });
 
+
+
 function setKeybinds(kitname){
     path = "Drum Samples/" + kitname + "/";
+    ipath = "Drum Icons/" + kitname + "/";
+    setIcons(ipath);
 }
+
